b837cc3bfae7efdb4b4fe3b0b5ac8d352cded5ec
SeedPaths-User <lessdismissive@email.com>
2015-03-16 09:17:23 -0600

    Updates to program
    Week7CodeChallenge

---
+++ Week7CodeChallenge/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Week7CodeChallenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Index
+        public ActionResult Index()
+        {
+            if (Request.IsAjaxRequest())
+                return PartialView();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {            
+                return PartialView();            
+        }
+
+        public ActionResult Careers()
+        {
+            return PartialView();
+        }
+
+        [HttpGet]
+        public ActionResult Contact()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(Models.Week7ChallengeContactsTable contact)
+        {
+            Models.sp6TimEntities2 db = new Models.sp6TimEntities2();
+
+            db.Week7ChallengeContactsTable.Add(contact);
+            db.SaveChanges();
+
+            return Content("Thank you for contacting us!");
+        }
+
+        public ActionResult Work()
+        {
+            return PartialView();
+        }
+        
+    }
+}
---
+++ Week7CodeChallenge/Scripts/Main.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+
+    $('body').on('click', '.ajax-link', function () {
+        var btnClicked = $(this); //If 'this' is used inside the GET block, it no longer refers to the button.
+        var btnID = $(this).attr('id') //Bottom navbar click should highligh proper top tab.
+
+        $.get($(this).data('url'), function (data) {
+            if (btnClicked.hasClass('submenu')) {
+                $('#subContainer').html(data);
+
+                $('#container').find('.ajax-link').removeClass('active'); //Remove Active class off of all buttons in Container
+                btnClicked.addClass('active'); //Add it to the clicked button.
+            }
+            else {
+                $('#container').html(data);
+                $('header').find('.ajax-link').removeClass('active'); //Remove Active class off of all buttons in the header
+                $('#' + btnID).addClass('active'); //Add it to the clicked button.
+
+            }
+        });
+    });
+
+    $('body').on('submit', '.ajax-form', function (event) {
+
+        //Preventing the default behaviour of a form.
+        event.preventDefault();
+
+        $.post($(this).data('posturl'), $(this).serialize(), function (data) {
+            //Update the content area.
+            $('#container').html(data);
+        });
+    });
+
+});
